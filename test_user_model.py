246ef0a3edfea8df80d265bc38ed413fdd5f5231
James <jrogers2fc@gmail.com>
2022-08-23 09:24:11 -0700

    user model tests

--- test_user_model.py
+++ test_user_model.py
@@ -7,6 +7,7 @@
 
 import os
 from unittest import TestCase
+from sqlalchemy import exc
 
 from models import db, User, Message, Follows
 
@@ -34,10 +35,19 @@ class UserModelTestCase(TestCase):
 
     def setUp(self):
         """Create test client, add sample data."""
+        db.drop_all()
+        db.create_all()
 
-        User.query.delete()
-        Message.query.delete()
-        Follows.query.delete()
+        userA = User.signup("testa", "email@gmail.com", "password", None)
+        userB = User.signup("testb", "email@gmail.com", "password", None)
+        userA.id = 1
+        userB.id = 2
+
+        db.session.commit()
+        userA = User.query.get(1)
+        userB = User.query.get(2)
+        self.userA = userA
+        self.userB = userB
 
         self.client = app.test_client()
 
@@ -56,3 +66,66 @@ class UserModelTestCase(TestCase):
         # User should have no messages & no followers
         self.assertEqual(len(u.messages), 0)
         self.assertEqual(len(u.followers), 0)
+    
+    def test_user_following(self):
+        self.userA.following.append(self.userB)
+        db.session.commit()
+
+        self.assertEqual(len(self.userB.following),0)
+        self.assertEqual(len(self.userA.following),1)
+        self.assertEqual(self.userA.following[0].id, self.userB.id)
+
+
+    def test_user_followers(self):
+        self.userA.following.append(self.userB)
+        db.session.commit()
+
+        self.assertEqual(len(self.userB.followers),1)
+        self.assertEqual(len(self.userA.followers),0)
+        self.assertEqual(self.userB.followers[0].id, self.userA.id)
+
+    def test_user_is_following(self):
+        self.userA.following.append(self.userB)
+        db.session.commit()
+
+        self.assertTrue(self.userA.is_following(self.userB))
+        self.assertFalse(self.userB.is_following(self.userA))
+
+    def test_user_create(self):
+        user = User.signup("testUser", "valid@email.com", "superqualitypassword", None)
+        user.id = 15120985
+        db.session.commit()
+
+        user = User.query.get(15120985)
+        self.assertEqual(user.username, "testUser")
+        self.assertEqual(user.email, "valid@email.com")
+        self.assertNotEqual(user.password, "superqualitypassword")
+
+    def test_user_create_badpassword(self):
+        user = User.signup("testUser", "valid@email.com", "", None)
+        user.id = 15120985
+        with self.assertRaises(exc.IntegrityError) as context:
+            db.session.commit()
+    
+    def test_user_create_bademail(self):
+        user = User.signup("testUser", None, "superqualitypassword", None)
+        user.id = 15120985
+        with self.assertRaises(exc.IntegrityError) as context:
+            db.session.commit()
+
+    def test_user_create_badusername(self):
+        user = User.signup(None, "valid@email.com", "", None)
+        user.id = 15120985
+        with self.assertRaises(exc.IntegrityError) as context:
+            db.session.commit()
+
+    def test_auth(self):
+        user = User.authenticate(self.userA.username, "password")
+        self.assertEqual(user.id, self.userA.id)
+
+    def test_auth_username(self):
+        self.assertFalse(User.authenticate("userC", "password"))
+    
+    def test_auth_password(self):
+        self.assertFalse(User.authenticate("userC", "password2"))
+    
